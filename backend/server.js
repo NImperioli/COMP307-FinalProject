83d094157b760eae6de66882da3fae17cacf879c
Nick <nicholas.imperioli@mail.mcgill.ca>
2026-03-23 12:43:48 -0400

    Reformatting and Contacted IT for Core Dump Fix

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./src/config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
