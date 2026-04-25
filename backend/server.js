7cfb47dd8979ff6d81eddd659387194214393d5e
Annie <155664992+Mai-Annie@users.noreply.github.com>
2026-04-25 16:30:26 -0400

    refactor: changing the api base to hardcode port 300

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 //app.use(cors({origin: 'http://FINAL.URL'})); // wb // change when site has its final url on mimi
 app.use(cors({
-  origin: 'http://winter2026-comp307-group38.cs.mcgill.ca',
+  origin: ['http://winter2026-comp307-group38.cs.mcgill.ca',
+           'https://winter2026-comp307-group38.cs.mcgill.ca'],
   allowedHeaders: ["Content-Type", "Authorization"]
 })); // wb
 // disable server-side cache, stop sending stale data
